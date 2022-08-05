26287084605444f3d1f87984dc7db50902d370fe
kenu <kenu.heo@gmail.com>
2022-08-05 13:55:29 +0900

    README

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # db-postgresql
+
+## Requirements
+
+- Node.js
+- postgresql
+
+## Config
+
+- copy `.env.sample` to `.env`
+- edit `.env` with your database connection details
+
+## Build and Run
+
+```bash
+yarn
+yarn start
+```
+
+* open http://localhost:3000/api/users
+
+## Test
+
+```bash
+yarn test
+```
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User, sequelize } = require("../models");
 
-router.get("/user", async (_req, res, _next) => {
+router.get("/users", async (_req, res, _next) => {
   const data = await User.findAll();
   res.json({ data: data });
 });
